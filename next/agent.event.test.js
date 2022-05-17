80fe974aa510ea811e4da197e80e65d1f9e4da0a
Simon Petrac <pfoerdie@gmail.com>
2022-05-17 15:56:30 +0200

    wip

--- next/agent.event.test.js
+++ next/agent.event.test.js
@@ -7,9 +7,19 @@ agent.on('type.method.step', console.log);
 const
     event = agent.createEvent({
         type:   'type.method.step',
-        source: 'agent.event'
+        // type:   'fua.module.weather.temperature.modified',
+        source: 'http://...',
+        datacontenttype: 'application/json',
+        // datacontenttype: 'text/turtle',
+        data: {
+            prov: 'agent.event',
+            value: '...',
+            tss: 0,
+            tsv: 0
+        }
     });
 
+// console.log(event);
 event.emit();
 // event.emit().emit();
 // event.emit(true).then(val => val.emit()).catch(console.error);
