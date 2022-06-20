a23415f4dc2f10880136fea54ffe3095d0d876f7
Simon Petrac <pfoerdie@gmail.com>
2022-06-20 15:53:29 +0200

    moved next code to current src

--- test/agent.event.test.js
+++ test/agent.event.test.js
@@ -1,21 +1,21 @@
 const
-    EventAgent = require('./agent.event.js'),
+    EventAgent = require('../src/agent.event.js'),
     agent      = new EventAgent();
 
 agent.on('type.method.step', console.log);
 
 const
     event = agent.createEvent({
-        type:   'type.method.step',
+        type: 'type.method.step',
         // type:   'fua.module.weather.temperature.modified',
-        source: 'http://...',
+        source:          'http://...',
         datacontenttype: 'application/json',
         // datacontenttype: 'text/turtle',
         data: {
-            prov: 'agent.event',
+            prov:  'agent.event',
             value: '...',
-            tss: 0,
-            tsv: 0
+            tss:   0,
+            tsv:   0
         }
     });
 
